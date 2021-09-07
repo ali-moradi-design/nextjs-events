197e0bab2d6cb7c659229adb9113d06937322015
Ali Moradi <a.moradi@homapharmed.local>
2021-09-07 16:30:14 +0430

    events pages

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,33 @@
-import React from "react";
+import Link from "next/Link";
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
-const EventsPage = () => {
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return {
+    props: { events },
+    revalidate: 1,
+  };
+}
+
+export default function EventsPage({ events }) {
   return (
     <Layout>
-      <h1>EventsPage</h1>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+        // <h3 key={evt.id}> {evt.name} </h3>
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
-};
-
-export default EventsPage;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,33 @@
-import Head from "next/head";
-import Link from "next/link";
+import Link from "next/Link";
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
-export default function Home() {
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return {
+    props: { events: events.slice(0, 3) },
+    revalidate: 1,
+  };
+}
+
+export default function Home({ events }) {
   return (
     <Layout>
-      <h1>Home page</h1>
-      <Link href="/about">go to about</Link>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+        // <h3 key={evt.id}> {evt.name} </h3>
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
